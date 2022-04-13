3056a54780cc07cf05b9d575aabfedeb74edcabb
nyaribari <anyaribari@gmail.com>
2022-04-13 10:57:35 +0300

    timed requests

---
+++ timeout.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+def timeOut_response():
+    '''
+    Python program to send a timed request request to a web page
+    ''' 
+    k = list(range(1,2))
+    for j in k:
+        rr = requests.get(f'https://api.openalex.org/authors?per-page=100&page={j}',timeout=0.05)
+        return json.loads(rr.content)
+        #return json.loads(rr.text)
+
+#invoke function
+print(timeOut_response())
